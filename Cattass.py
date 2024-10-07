7c370a78ee477b6602f3f7476db06f5efef716b4
Di <dimas2343@mail.ru>
2024-10-07 13:47:55 +0300

    Добавлен комбобокс для выбора тегов. Поле ввода удалено

--- Cattass.py
+++ Cattass.py
@@ -1,7 +1,10 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+Allowed_tags = ["sleep", "jump", "fight", "black", "white", "siamese", "cute"]
 
 def load_image(url):
     try:
@@ -15,11 +18,17 @@ def load_image(url):
         print(f"Произошла ошибка {e}")
         return None
 
-def set_image():
-    img = load_image(url)
+def open_new_window():
+    tag = tag_conbobox.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
+    img = load_image(url_tag)
 
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title("Картинка с котиком")
+        new_window.geometry("600x400")
+        label = Label(new_window, image = img)
+        label.pack()
         label.image = img
 
 def exit():
@@ -27,10 +36,10 @@ def exit():
 
 window = Tk()
 window.title("Cats")
-window.geometry("600x435+600+200")
+window.geometry("300x150+600+200")
 
-label = Label()
-label.pack()
+# tag_entry = Entry()
+# tag_entry.pack()
 
 # update_butoon = Button(text="Обновить", command=set_image)
 # update_butoon.pack()
@@ -40,13 +49,19 @@ window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label="Выход",command=exit)
 
-
 url = "https://cataas.com/cat"
 
-set_image()
+tag_label = Label(text="Выберите тег")
+tag_label.pack()
+
+tag_conbobox = ttk.Combobox(values=Allowed_tags)
+tag_conbobox.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 window.mainloop()
