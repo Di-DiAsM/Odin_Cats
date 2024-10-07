d10d5a043e8ef8edc6690697c70b321009457fef
Di <dimas2343@mail.ru>
2024-10-07 13:03:12 +0300

    Вместо кнопки добавлено меню

--- Cattass.py
+++ Cattass.py
@@ -22,6 +22,9 @@ def set_image():
         label.config(image=img)
         label.image = img
 
+def exit():
+    window.destroy()
+
 window = Tk()
 window.title("Cats")
 window.geometry("600x435+600+200")
@@ -29,8 +32,18 @@ window.geometry("600x435+600+200")
 label = Label()
 label.pack()
 
-update_butoon = Button(text="Обновить", command=set_image)
-update_butoon.pack()
+# update_butoon = Button(text="Обновить", command=set_image)
+# update_butoon.pack()
+
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Выход",command=exit)
+
 
 url = "https://cataas.com/cat"
 
